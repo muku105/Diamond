f6653bdef7967c1327215d270286c61d432a7046
Mukesh Tiwari <tiwarimukesh921@gmail.com>
2021-05-25 19:09:50 +0530

    Updates

---
+++ precache-manifest.ca3ce09ee6dd776623d650c5bd689035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903305e6f19c00551d18ddb25df9ef8c",
+    "url": "/Diamond/index.html"
+  },
+  {
+    "revision": "8ca5c2c453830d4776c6",
+    "url": "/Diamond/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "5c805e8ef09fad8e5530",
+    "url": "/Diamond/static/js/2.e4516fde.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Diamond/static/js/2.e4516fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca5c2c453830d4776c6",
+    "url": "/Diamond/static/js/main.c7397d9b.chunk.js"
+  },
+  {
+    "revision": "cf03ea6d818a70ba807d",
+    "url": "/Diamond/static/js/runtime-main.06b8f5e4.js"
+  }
+]);
